16a21b47835fc726f29e8d14e8e10357f2e086fc
Rita Cunha <rita.cunha.ts@gmail.com>
2022-02-13 10:59:43 +0000

    finished lab

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.set("view engine", "hbs");
 app.set("views", __dirname + "/views");
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public/"));
 
 // setting the spotify-api goes here:
 const spotifyApi = new SpotifyWebApi({
